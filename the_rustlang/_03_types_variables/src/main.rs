713bdc6987e4dfd904c97a180dd4f28e1338ceb6
augustine <augustino0890@gmail.com>
2020-08-02 23:40:55 +0900

    Operators

--- the_rustlang/_03_types_variables/src/main.rs
+++ the_rustlang/_03_types_variables/src/main.rs
@@ -1,6 +1,7 @@
 use std::mem;
 
-fn main() {
+#[allow(dead_code)]
+fn core_types() {
     // unsigned 0 + (255)
     let a:u8 = 123; // 8bits
     println!("a = {}", a);
@@ -32,4 +33,47 @@ fn main() {
     println!("g = {}, size = {} bytes.", g, mem::size_of_val(&g));
     let f = 7 > 0;
     println!("f = {}, size = {} bytes.", f, mem::size_of_val(&f));
+}
+
+fn operators() {
+    // arithmetic
+    let mut a = 2 + 3*4;
+    println!("a = {}", a);
+    a = a + 1;
+    println!("a = {}", a);
+    a += 2; // -= += *= /= %=
+    println!("a = {}", a);
+
+    println!("The remainder of {} / {} = {}", a, 4, (a%4));
+
+    let a_cubed = i32::pow(a, 3);
+    println!("a_cubed = {}", a_cubed);
+
+    let b = 2.5;
+    let b_cubed = f64::powi(b, 3);
+    let b_to_pi = f64::powf(b, std::f64::consts::PI);
+    println!("b_cubed = {}", b_cubed);
+    println!("b_to_pi = {}", b_to_pi);
+
+    // bitwise
+    // | OR & AND ^ XOR ! NOR
+    let c = 1 | 2; // 01 OR 10 = 11 == 3_10
+    println!("c = {}", c);
+    let two_to_10 = 1 << 10;
+    println!("2^10 = {}", two_to_10);
+
+    // logical
+    let pi_less_4 = std::f64::consts::PI < 4.0; // true
+    println!("Does PI less than 4.0: {}", pi_less_4);
+    // > <= >= ==
+    let x = 5;
+    let x_is_5 = x == 5; // true
+    println!("Does x less than 5: {}", x_is_5);
+}
+
+fn main() {
+    // Core Data Types
+    // core_types();
+
+    operators();
 }
