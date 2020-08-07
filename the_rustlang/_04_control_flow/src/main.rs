8b069e51b728b78cf3c3a59a9aed63c02477962d
augustine <augustino0890@gmail.com>
2020-08-07 19:06:35 +0900

    While and loop

--- the_rustlang/_04_control_flow/src/main.rs
+++ the_rustlang/_04_control_flow/src/main.rs
@@ -24,6 +24,29 @@ fn if_statement() {
     );
 }
 
+#[allow(dead_code)]
+fn while_and_loop() {
+    let mut x = 1;
+    while x < 100 {
+        x *= 2;
+        if x == 64 { continue; }
+        println!("x = {}", x);
+    }
+
+    let mut y = 1;
+    loop { // while true
+        y *= 2;
+        println!("y = {}", y);
+        
+        if y == 1<<10 { break; }
+    }
+}
+
+
 fn main() {
-    if_statement();
+    // If statement
+    // if_statement();
+
+    // While and loop
+    while_and_loop();
 }
